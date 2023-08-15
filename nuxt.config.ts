fb66399ddec9ed401add41ce162db910ef47c66e
Mahdi Shah Abbasian <mehdi.shahabbasian@gmail.com>
2023-08-15 22:57:22 +0330

    Required project plugins have been installed.
    پلاگین‌های مورد نیاز پروژه نصب شد.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    head: {
+      title: 'Comics Character',
+    }
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@formkit/nuxt',
+    '@pinia/nuxt'
+  ],
+  tailwindcss: {
+    viewer: false
+  }
 })
