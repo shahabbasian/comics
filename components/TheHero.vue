bc29fe3806e23664742bd19cde7d112aa6441e52
Mahdi Shah Abbasian <mehdi.shahabbasian@gmail.com>
2023-08-15 23:26:12 +0330

    Homepage hero section has been added.
    هیرو صفحه اصلی اضافه شد.

---
+++ components/TheHero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="hero h-[40vh]"
+    style="background-image: url(https://cdn.marvel.com/content/1x/clean_462.jpg);"
+  >
+    <div class="hero-overlay bg-opacity-60" />
+    <div class="hero-content text-center text-white">
+      <div class="max-w-xl">
+        <h1 class="mb-5 text-5xl font-bold">
+          This week's new comics
+        </h1>
+        <p class="mb-5">
+          Meet the Dark X-Men, investigate the mystery of Captain Krakoa, see Iron Man battle Storm, and more in this week's comics!
+        </p>
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <TheHero>
+      Search
+    </TheHero>
+  </div>
+</template>
