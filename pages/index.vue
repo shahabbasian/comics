586e417f0a3be4fac95231f663df0bf2d7e53f84
Mahdi Shah Abbasian <mehdi.shahabbasian@gmail.com>
2023-08-15 23:50:30 +0330

    Added the appearance of the search functionality.
    ظاهر جستجو اضافه شد.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
     <TheHero>
-      Search
+      <div class="join join-vertical md:join-horizontal">
+        <div>
+          <div>
+            <input
+              class="input input-bordered join-item"
+              placeholder="Search Character"
+            >
+          </div>
+        </div>
+
+        <select class="select select-bordered join-item">
+          <option selected>
+            Name
+          </option>
+          <option>Name starts with</option>
+        </select>
+
+        <button class="btn join-item">
+          Search
+        </button>
+      </div>
     </TheHero>
   </div>
 </template>
