7f800efc2c61665067cdbc8a8c92a32fa5e025cf
Mahdi Shah Abbasian <mehdi.shahabbasian@gmail.com>
2023-08-15 23:37:08 +0330

    Increased opacity of the hero section and improved spacing between code blocks for better readability.
    opacity هیرو بیشتر و فاصله بین بلاک‌های کد خواناتر شد.

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -3,15 +3,18 @@
     class="hero h-[40vh]"
     style="background-image: url(https://cdn.marvel.com/content/1x/clean_462.jpg);"
   >
-    <div class="hero-overlay bg-opacity-60" />
+    <div class="hero-overlay bg-opacity-80" />
+    
     <div class="hero-content text-center text-white">
       <div class="max-w-xl">
-        <h1 class="mb-5 text-5xl font-bold">
+        <h1 class="mb-5 text-4xl font-bold">
           This week's new comics
         </h1>
+
         <p class="mb-5">
           Meet the Dark X-Men, investigate the mystery of Captain Krakoa, see Iron Man battle Storm, and more in this week's comics!
         </p>
+
         <slot />
       </div>
     </div>
