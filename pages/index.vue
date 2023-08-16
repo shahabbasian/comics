670ecd1e5ade589d85573fd5c3dd32a5af14d1ea
Mahdi Shah Abbasian <mehdi.shahabbasian@gmail.com>
2023-08-16 10:46:36 +0330

    Added the skeleton for the card.
    اسکلت مربوط به کارد اضافه شد.

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,14 @@
     <TheHero>
       Search
     </TheHero>
+
+    <div class="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
+      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <SkeletonTheCard
+          v-for="item in 8"
+          :key="item"
+        />
+      </div>
+    </div>
   </div>
 </template>
