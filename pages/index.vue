63ba61a7299fcd9b11ce68e9d43e745fd754c9f9
Mahdi Shah Abbasian <mehdi.shahabbasian@gmail.com>
2023-08-17 09:56:14 +0330

    Fetched and displayed character data from the API on the homepage.
    دیتای مربوط به کاراکترها در صفحه اصلی از API دریافت و نمایش داده ‌می‌شود

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+const { data, pending, refresh } = await useAsyncData('characters', () => useApiRequest(getCharacters()), {
+  transform: (data: object) => {
+    return {
+      pagination: {
+        offset: data.data.offset,
+        limit: data.data.limit,
+        total: data.data.total,
+        count: data.data.count
+      },
+      results: data.data.results.map((item: object) => {
+        return {
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          image: `${item.thumbnail.path}.${item.thumbnail.extension}`
+        }
+      })
+    }
+  }
+})
+</script>
+
 <template>
   <div>
     <TheHero>
@@ -5,16 +28,41 @@
     </TheHero>
 
     <div class="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
-      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-        <TheCard
-          image="https://cdn.marvel.com/content/1x/clean_462.jpg"
-          name="3-D Man"
-        />
+      <div
+        class="mb-6"
+        @click="refresh"
+      >
+        Refresh
+      </div>
+
+      <div
+        v-if="pending"
+        class="custom-grid"
+      >
         <SkeletonTheCard
           v-for="item in 8"
           :key="item"
         />
       </div>
+
+      <div
+        v-else
+        class="custom-grid"
+      >
+        <TheCard
+          v-for="item in data.results"
+          :key="item.id"
+          :image="item.image"
+          :name="item.name"
+          :description="item.description"
+        />
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.custom-grid {
+  @apply grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8
+}
+</style>
